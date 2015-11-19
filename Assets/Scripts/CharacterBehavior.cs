6dd973b4d860d303eb746b8b999e6545b7b1c16c
Vic vD <vicvanduynenpro@gmail.com>
2015-11-19 19:01:34 +0100

    First Script, House and Character

---
+++ Assets/Scripts/CharacterBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterBehavior : MonoBehaviour
+{
+
+	public int sens;
+	public float speed;
+    private Vector2 posXY;
+
+    void Awake()
+    {
+        posXY = new Vector2(transform.position.x, transform.position.y);
+    }
+
+	private void Move ()
+	{
+        posXY.x += sens * speed;
+        transform.position = new Vector2(posXY.x, transform.position.y);
+	}
+
+    void Update()
+    {
+        Move();
+
+    }
+
+
+}
