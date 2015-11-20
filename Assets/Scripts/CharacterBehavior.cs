b5823d635d5344b47cbd67e79aae4707e45e49d2
Vic vD <vicvanduynenpro@gmail.com>
2015-11-20 01:52:45 +0100

    Character Movement + Start of Character Generator

--- Assets/Scripts/CharacterBehavior.cs
+++ Assets/Scripts/CharacterBehavior.cs
@@ -6,23 +6,50 @@ public class CharacterBehavior : MonoBehaviour
 
 	public int sens;
 	public float speed;
-    private Vector2 posXY;
 
+    private Vector3 posXY;
+    private Animator animator;
+
+    private Animation _animation;
+    private float clipLength;
+    private bool goingIn;
     void Awake()
     {
-        posXY = new Vector2(transform.position.x, transform.position.y);
+        posXY = new Vector3(transform.position.x, transform.position.y,0);
+        animator = transform.GetComponentInChildren<Animator>();
+        
+       
     }
 
 	private void Move ()
 	{
         posXY.x += sens * speed;
-        transform.position = new Vector2(posXY.x, transform.position.y);
+        transform.position = new Vector3(posXY.x, transform.position.y,0);
 	}
+
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.tag == "House" && !goingIn)
+        {
+            sens = 0;
+            animator.SetBool("Open", true);
+            clipLength = animator.GetCurrentAnimatorStateInfo(0).length;
+            Debug.Log(clipLength);
+            StartCoroutine(GoIn());
+            goingIn = true;
+        }
+    }
+
+    IEnumerator GoIn()
+    {
+        yield return new WaitForSeconds(clipLength);
+        Destroy(this.gameObject);
+    }
 
     void Update()
     {
         Move();
-
     }
 
 
---
+++ Assets/Scripts/CharacterGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class CharacterGenerator : MonoBehaviour {
+
+    public List<GameObject> CharacterTypes;
+    public Vector2 chPos;
+
+    void Awake()
+    {
+
+    }
+
+    void CreateCharacter()
+    {
+        GameObject createdGameObject = Instantiate(CharacterTypes[Random.Range(0, CharacterTypes.Count)]);
+        
+    }
+
+    IEnumerator TimeToCreate()
+    {
+        yield return new WaitForSeconds(Random.Range(5.0f, 10.0f));
+    }
+
+
+}
