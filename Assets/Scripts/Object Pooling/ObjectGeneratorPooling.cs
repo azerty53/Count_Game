652ff0bf73b1e0382e0a7e44213e80fa9f5f827b
Vic vD <vicvanduynenpro@gmail.com>
2015-11-23 01:15:23 +0100

    Test

---
+++ Assets/Scripts/Object Pooling/ObjectGeneratorPooling.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectGeneratorPooling : MonoBehaviour {
+
+    private static ObjectGeneratorPooling instance;
+    protected ObjectGeneratorPooling() { }
+    public static ObjectGeneratorPooling Instance
+    {
+        get
+        {
+            if (instance == null) instance = GameObject.FindObjectOfType(typeof(ObjectGeneratorPooling)) as ObjectGeneratorPooling;
+            return instance;
+        }
+
+    }
+
+
+
+
+
+
+}
